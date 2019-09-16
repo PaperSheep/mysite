d6ebcaca32e28a5751d930612b48c6c73d215ddb
PaperSheep <39491945+PaperSheep@users.noreply.github.com>
2019-09-17 02:37:50 +0800

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -179,6 +179,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.qq.com'
 EMAIL_PORT = 25
 EMAIL_HOST_USER = '1009304154@qq.com'
-EMAIL_HOST_PASSWORD = 'hoglqostdjaqbedc'  # 授权码
+EMAIL_HOST_PASSWORD = ''  # 授权码
 EMAIL_SUBJECT_PREFIX = '[晓纸然的博客] '
 EMAIL_USE_TLS = True  # 与SMTP服务器通信时，是否启动TLS链接(安全链接)
